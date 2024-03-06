3fa51dd209f230519ab2ef4e546f98be7fbaa78d
masoud ahmadi <masoud.ahmadi@estdco.com>
2024-03-06 19:28:58 +0330

    Class session 4

--- ClassCourse.m
+++ ClassCourse.m
@@ -149,3 +149,122 @@
 %      disp(n)
 %      n = n-1
 %  end
+%%
+% ایجاد یک ماتریس تصادفی بین یک بازه
+% اولین ارگومان حدود را مشخص میکند مثلا بین 1 تا 10
+% دومین ارگومان تعداد سطر ها 
+% سومی تعداد ستون ها
+% x = randi([1 10], 1, 1);
+% 
+%% مثال حدس عدد با حلقه 
+% while true
+%     ournumber = input("Enter a number:");
+%     if (ournumber > x)
+%         disp("select a smaller one");
+%     elseif (ournumber < x)
+%         disp("select a bigger one");
+%     elseif (ournumber == x)
+%         disp("you win");
+%         break;
+%     end
+% end
+%% مثال احتمال رو یا پشت افتادن سکه
+% ro = 0;
+% posht = 0;
+% 
+% for c = 1:10000000
+%     num = randi([1 2],1,1);
+%     if(num == 1)
+%         ro = ro +1;
+%     elseif (num==2)
+%         posht = posht +1;
+%     end
+% end
+% 
+% percent = ro / (ro + posht);
+% disp(percent)
+%% مثال حلقه تو در تو
+% a = input("Enter a number: ");
+% nRow = a;
+% nCol = a;
+% p = zeros(nRow,nCol);
+% 
+% for x = 1:nRow
+%     for y = 1:nCol
+%         if x== y
+%             p(x,y) = -1;
+%         elseif ((x == 1 & y == a) |  (x== a & y == 1))
+%             p(x,y) = 10;
+%         end
+%     end
+% end
+% 
+% p
+%% نحوه استفاده از subplot
+% نمایش چندین گراف در یک شکل
+% نحوه تعریف استایل رنگ و مارکر برای هر گراف
+% x = 1:10;
+% y = 10:19;
+% y2 = 1:10;
+% subplot(2,2,1)
+% plot(x,y, "-+b")
+% 
+% subplot(2,2,4)
+% plot(x,y2,"--or")
+%% تمرین عدد پی با حلقه for
+% num = 1000000;
+% inCircle = 0;
+% for c = 1: num
+%     x = rand(1);
+%     y = rand(1);
+%     if x^2 + y^2 <=1
+%         inCircle = inCircle + 1;
+%     end
+% end
+%% نحوه استفاده از عملیات ماتریس به جای حلقه 
+% num = 1000000;
+% x = rand(1,num);
+% y = rand(1,num);
+% 
+% z = x.^2 + y.^2 <=1;
+% inCircle = sum(z);
+% disp(4 * (inCircle / num))
+
+%% نحوه تعریف یک فانکشن 
+% از تب هوم گزینه نیو فانکشن
+% کلمه کلیدی function
+% خروجی های متد به صورت ماتریس
+% =
+% نام متد
+% ورودی های متد
+% function [outputArg1,outputArg2] = NameForFunction(inputArg1,inputArg2)
+% %UNTITLED Summary of this function goes here
+% %   Detailed explanation goes here
+% outputArg1 = inputArg1;
+% outputArg2 = inputArg2;
+% end
+
+%% مثال محاسبه میانگین
+% function [avg] = CalcAvg(num1)
+% avg= sum(num1) / length(num1);
+% end
+%% مثال جمع اعداد تا عدد مشخص
+% function [s] = SumToNum(num)
+% s = 0;
+% for c= 1:2:num
+%     s = s + c; 
+% end
+%% حل مثال اول اولر پراجکت 
+% همه اعداد بخش پذیر بر 3 یا 5 کوچکتر از یک عدد مشخص
+% function [res] = EulerPrj1(num)
+% res = 0;
+% for c = 1 : num -1
+%     if(mod(c,3)==0 || mod(c,5) ==0)
+%         res = res +c;
+%     end
+% end
+% end
+
+
+
+
